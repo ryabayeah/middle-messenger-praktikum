d5ed9b5fc23bfd23b33afbfef24025c52a2640f5
Анастасия Абазовская <ryabayeah@gmail.com>
2024-02-11 18:24:06 +0700

    fix indent

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,12 +8,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + "/dist"));
 
-app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+app.get("*", (_, res) => {
+	res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 app.listen(PORT, function () {
-    console.log(`Example app listening on port ${PORT}!`);
+	console.log(`Example app listening on port ${PORT}!`);
 });
